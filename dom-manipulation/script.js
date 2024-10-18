cfd9f18adcd85a3fa9fb1b900699af9a183e5dd5
karltayy <karltay52@gmail.com>
2024-10-18 11:40:45 +0000

    js update

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -251,9 +251,5 @@ let quotes = JSON.parse(localStorage.getItem('quotes')) || [
   function exportQuotesToJson() {
     const dataStr = JSON.stringify(quotes, null, 2);
     const blob = new Blob([dataStr], { type: "application/json" });
-    const url = URL.createObjectURL(blob);
-  
-    const a = document.createElement('a');
-    a.href = url;
-   
+    const url = URL.createObjectURL(blob
   
